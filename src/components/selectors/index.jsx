fcb34e5e16167a0399b8573de0b616ada1200d48
Minh Bao <minhbao@Minhs-Laptop.local>
2025-06-23 21:37:35 +0700

    remove upload button

--- src/components/selectors/index.jsx
+++ src/components/selectors/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import FrameSelector from "./frame-selector";
 import DelaySelector from "./delay-selector";
-import ImageSelector from "./image-selector";
+// import ImageSelector from "./image-selector";
 
 const Selectors = () => {
   return (
     <div className="flex items-center gap-2">
       <FrameSelector />
       <DelaySelector />
-      <ImageSelector />
+      {/* <ImageSelector /> */}
     </div>
   );
 };
